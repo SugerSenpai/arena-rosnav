a67aec593bd43e7aa9858d0404d68c5b21db5d48
Vova Shcherbyna <volodymyr.shcherbyna@alumni.tu-berlin.de>
2023-11-15 15:08:43 +0100

    cleanup

--- utils/ros/rosnodes/pedsim_waypoint_plugin/pedsim_waypoint_plugin/__init__.py
+++ utils/ros/rosnodes/pedsim_waypoint_plugin/pedsim_waypoint_plugin/__init__.py
@@ -1,5 +1,3 @@
 from .plugins.spinny import main # noqa
 from .plugins.passthrough import main # noqa
-from .plugins.rvo import main#noqa
-from .plugins.rvo2 import main#noqa
 from .plugins.orca import main
--- utils/ros/rosnodes/pedsim_waypoint_plugin/pedsim_waypoint_plugin/plugins/orca/main.py
+++ utils/ros/rosnodes/pedsim_waypoint_plugin/pedsim_waypoint_plugin/plugins/orca/main.py
@@ -12,34 +12,33 @@ class Plugin_Orca(WaypointPlugin):
 
     def callback(self, data) -> OutputData:
         
-        def datapoint_to_vec(agent: pedsim_msgs.msg.AgentState) -> pedsim_msgs.msg.AgentFeedback:
-
-            angle = agent.direction
-            
-            #velocity = np.linalg.norm(np.array([agent.twist.linear.x, agent.twist.linear.y]))
-            velocity = self.diameter / 2
-            feedback = pedsim_msgs.msg.AgentFeedback()
-	     
-            feedback.id = agent.id
-            # let the agent walk in a semicircle
-            angle = np.pi * self.simulator.getGlobalTime() / (2*np.pi)  # Change from 0 to pi over approx. one time period
-            feedback.force.x = np.cos(angle) * velocity
-            feedback.force.y = np.sin(angle) * velocity
-
+        for agent in data.agents:
             # If agent not in simulator, add it and process any obstacles
             if agent.id not in self.agent_id_map.keys():
-                self.agent_id_map[agent.id] = self.simulator.addAgent((agent.pose.position.x, agent.pose.position.y), 1.5, 5, 1.5, 2, 0.4, 2, (feedback.force.x, feedback.force.y))
-                self.simulator.processObstacles()
+                self.agent_id_map[agent.id] = self.simulator.addAgent((agent.pose.position.x, agent.pose.position.y), 1.5, len(data.agents) - 1, 1.5, 2, 0.4, 0.4, (0,0))
             
-            # Set the calculated force as the preferred velocity of the agent in the simulator
-            self.simulator.setAgentPrefVelocity(self.agent_id_map[agent.id], (feedback.force.x, feedback.force.y))
-
-            return feedback
-
+            self.simulator.processObstacles()
+            self.simulator.setAgentPrefVelocity(self.agent_id_map[agent.id], (agent.destination.x - agent.pose.position.x, agent.destination.y - agent.pose.position.y))
+            
+            
         # Perform simulation step
         self.simulator.doStep()
+
+        def get_feedback(agent: pedsim_msgs.msg.AgentState) -> pedsim_msgs.msg.AgentFeedback:
+            res = pedsim_msgs.msg.AgentFeedback()
+
+            agent_id = self.agent_id_map.get(agent.id)
+
+            force = self.simulator.getAgentVelocity(agent_id)
+
+            res.id = agent.id
+            res.force.x, res.force.y = force
+            res.force.z = 0
+            res.unforce = False
+
+            return res
         
-        return [datapoint_to_vec(agent) for agent in data.agents]
+        return [get_feedback(agent) for agent in data.agents]
     
     
 
